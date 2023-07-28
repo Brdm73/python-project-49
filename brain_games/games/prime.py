387417a29d72e0bc0684c8d8edece2d3dd557761
Khnyrev <aleksey.khnyrev@dualbootpartners.com>
2023-07-28 23:23:15 +0400

    the prime module has a dedicated function that checks whether a number is prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,14 +1,19 @@
 import random
 
-
 GAME_LOGO = "brain-prime\n"
 GAME_QUESTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
+def is_prime(num):
+    counter = 0
+    for i in range(1, num + 1):
+        if num % i == 0:
+            counter += 1
+    return True if counter == 2 else False
+
+
 def generate_question():
     question = random.randint(1, 100)
-    counter = 0
-    for i in range(1, question + 1):
-        if question % i == 0:
-            counter += 1
-    return question, "yes" if counter == 2 else "no"
+    correct_answer = "yes" if is_prime(question) else "no"
+
+    return question, correct_answer
